55ceeaa703ec4a4f9610bed77bc498d2aafe8a75
github_asadmau <asadulkon@gmail.com>
2021-03-20 01:05:26 +0700

    Update .gitignore

--- request.py
+++ request.py
@@ -1,1 +1,13 @@
 import requests
+
+ac_id = 'https://learn.stikombanyuwangi.ac.id'
+try:
+    r = requests.get(ac_id)
+    if r.status_code == 200:
+        print(f'Success ! status code {r}')
+        print('Content ',r.text)
+    else:
+        print(f'terjadi kesalahan di !{r.status_code}')
+except Exception as e:
+    print('error 404')
+print('end code')
